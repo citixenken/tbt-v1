16883d4f7473cfec72e2c70e4829c2c4e298b18d
citizen-ken <citizen-ken@users.noreply.github.com>
2016-03-07 12:41:17 +0300

    re-wording

--- controllers/contact.js
+++ controllers/contact.js
@@ -37,7 +37,7 @@ exports.postContact = function(req, res) {
   var name = req.body.name;
   var body = req.body.message;
   var to = 'your@email.com';
-  var subject = 'Contact Form | Hackathon Starter';
+  var subject = 'Contact Form | Team-Book-Tu';
 
   var mailOptions = {
     to: to,
